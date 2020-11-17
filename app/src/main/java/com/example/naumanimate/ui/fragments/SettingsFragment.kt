e512bba3b04e6d60468ef1afeb9726b5242d4a00
almirnaumov <67364736+almirnaumov@users.noreply.github.com>
2020-11-17 21:33:22 +0500

    6. Добавляю Options menu с иконками

--- app/src/main/java/com/example/naumanimate/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/naumanimate/ui/fragments/SettingsFragment.kt
@@ -1,10 +1,8 @@
 package com.example.naumanimate.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.naumanimate.R
 import com.example.naumanimate.databinding.FragmentSettingsBinding
 import com.example.naumanimate.databinding.FragmentTheoryBinding
@@ -16,5 +14,10 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+    activity?.menuInflater?.inflate(R.menu.settings_action_menu,menu)
     }
 }
